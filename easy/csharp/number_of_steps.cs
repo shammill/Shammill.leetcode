421a0ff3385c165006157e5971b621ad9f94d95b
Samuel Hammill <shammill88@gmail.com>
2023-05-01 16:38:42 +1000

    rename folder, add another challenge

---
+++ easy/csharp/number_of_steps.cs
@@ -0,0 +1,20 @@
+// 1342. Number of Steps to Reduce a Number to Zero
+// Given an integer num, return the number of steps to reduce it to zero.
+// In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+public class Solution
+{
+    public int NumberOfSteps(int num)
+    {
+       int count = 0;
+       while (num > 0)
+       {
+            if (num % 2 != 0)
+                num = num / 2;
+            else
+                num--;
+            count++;
+        }
+        return count;
+    }
+}
